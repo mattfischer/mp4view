1def7ab3ce6474145487ea1514a74bcf4938fbc2
Matt Fischer <mattfischer84@gmail.com>
2026-04-14 22:26:37 -0600

    Break spectrum view into raw and full tabs

--- analyzers/mp4/analyzer.py
+++ analyzers/mp4/analyzer.py
@@ -7,15 +7,57 @@ from syntax import SyntaxView
 
 from PySide2 import QtWidgets, QtGui
 
-class AACSpectrumPlot(QtWidgets.QWidget):
-    def __init__(self):
-        super(AACSpectrumPlot, self).__init__()
-        self.spectrum_data = []
-        self.ics = None
+class AACSpectrumScalefactorPlot(QtWidgets.QWidget):
+    def __init__(self, channel):
+        super(AACSpectrumScalefactorPlot, self).__init__()
+        self.channel = channel
+        self.aac = None
 
-    def set_spectrum(self, spectrum_data, ics):
-        self.spectrum_data = spectrum_data
-        self.ics = ics
+    def set_aac(self, aac):
+        self.aac = aac
+        self.update()
+
+    def paintEvent(self, event):
+        painter = QtGui.QPainter(self)
+        painter.drawRect(0, 0, self.width() - 1, self.height() - 1)
+
+        ics = self.aac.block.cpe.ics[self.channel]
+
+        prev = None
+        brush = QtGui.QBrush(QtGui.QColor(192, 192, 192))
+        pen = QtGui.QPen(QtGui.QBrush(QtGui.QColor(128, 176, 224)), 1)
+        painter.setPen(pen)
+        
+        for sfb in range(ics.ics_info.max_sfb):
+            start = ics.params.swb_offset[sfb]
+            end = ics.params.swb_offset[sfb+1]
+            val = ics.scale_factor_data.sf[0][sfb]
+
+            sx = self.width() * start / 512
+            ex = self.width() * end / 512
+            w = ex - sx - 1
+            h = self.height() * val/256
+            painter.fillRect(sx, self.height() - h, w, h, brush)
+
+            for bin in range(start, end):
+                s = ics.spectral_data.spec[0][0][sfb][bin - start]
+                x = self.width() * bin / 512
+                y = self.height() * (1 + s / 32) / 2
+                if prev:
+                    (prev_x, prev_y) = prev
+                    painter.drawLine(prev_x, prev_y, x, y)
+                prev = (x, y)
+
+        painter.end()
+
+class AACSpectrumPlot(QtWidgets.QWidget):
+    def __init__(self, channel):
+        super(AACSpectrumPlot, self).__init__()
+        self.channel = channel
+        self.aac = None
+
+    def set_aac(self, aac):
+        self.aac = aac
         self.update()
 
     def paintEvent(self, event):
@@ -51,25 +93,17 @@ class AACSpectrumPlot(QtWidgets.QWidget):
             w = self.width() * i / 512
             painter.drawLine(w, 0, w, self.height())
 
-        brush = QtGui.QBrush(QtGui.QColor(192, 192, 192))
-        for sfb in range(self.ics.ics_info.max_sfb):
-            start = self.ics.params.swb_offset[sfb]
-            end = self.ics.params.swb_offset[sfb+1]
-            val = self.ics.scale_factor_data.sf[0][sfb]
+        ics = self.aac.block.cpe.ics[self.channel]
 
-            sx = self.width() * start / 512
-            ex = self.width() * end / 512
-            w = ex - sx - 1
-            h = self.height() * val/256
-            painter.fillRect(sx, self.height() - h, w, h, brush)
+        spectrum_data = self.aac.spec[self.channel]
 
         prev = None
-        num = len(self.spectrum_data[0][0])
+        num = len(spectrum_data[0][0])
         pen = QtGui.QPen(QtGui.QBrush(QtGui.QColor(128, 176, 224)), 1)
         painter.setPen(pen)
         for i in range(num):
             x = self.width() * i / num
-            s = self.spectrum_data[0][0][i]
+            s = spectrum_data[0][0][i]
             sign = 1 if s > 0 else -1
             y = self.height() * (1 - sign * math.log(1 + abs(s), 10) / 7) / 2
             if prev:
@@ -78,24 +112,24 @@ class AACSpectrumPlot(QtWidgets.QWidget):
             prev = (x, y) 
         painter.end()
 
-class AACSpectrumView(QtWidgets.QScrollArea):
-    def __init__(self):
-        super(AACSpectrumView, self).__init__()
+class AACPerChannelView(QtWidgets.QScrollArea):
+    def __init__(self, cls):
+        super(AACPerChannelView, self).__init__()
 
         self.widget = QtWidgets.QWidget()
         self.spectrum_plots = []
         layout = QtWidgets.QVBoxLayout()
         for i in range(2):
-            plot = AACSpectrumPlot()
+            plot = cls(i)
             layout.addWidget(plot)
             self.spectrum_plots.append(plot)
         self.widget.setLayout(layout)
         self.widget.setMinimumSize(500, 500)
         self.setWidget(self.widget)
 
-    def set_spectrum(self, spectrum_data, ics):
-        for i in range(2):
-            self.spectrum_plots[i].set_spectrum(spectrum_data[i], ics[i])
+    def set_aac(self, aac):
+        for plot in self.spectrum_plots:
+            plot.set_aac(aac)
 
     def resizeEvent(self, event):
         self.widget.setFixedSize(event.size().width(), event.size().width())
@@ -119,7 +153,9 @@ class AACStreamView(QtWidgets.QWidget):
         tabs = QtWidgets.QTabWidget()
         self.syntax_view = SyntaxView('Syntax')
         tabs.addTab(self.syntax_view, self.syntax_view.title)
-        self.spectrum_view = AACSpectrumView()
+        self.spec_sf_view = AACPerChannelView(AACSpectrumScalefactorPlot)
+        tabs.addTab(self.spec_sf_view, 'Raw Spectrum/Scalefactors')
+        self.spectrum_view = AACPerChannelView(AACSpectrumPlot)
         tabs.addTab(self.spectrum_view, 'Spectrum')
 
         vlayout.addWidget(tabs)
@@ -136,7 +172,8 @@ class AACStreamView(QtWidgets.QWidget):
         self.syntax_view.update_syntax(self.file.bytestream, self.aac.syntax_items())
         self.syntax_view.set_highlight(location, len(bytes))
 
-        self.spectrum_view.set_spectrum(self.aac.spec, self.aac.block.cpe.ics)
+        self.spec_sf_view.set_aac(self.aac)
+        self.spectrum_view.set_aac(self.aac)
 
 class Analyzer:
     def __init__(self, stream):
