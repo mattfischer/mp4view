e96841047cfa26f4c188f7c8152b9fe61d50669e
Matt Fischer <mattfischer84@gmail.com>
2026-05-14 22:57:32 -0600

    More slice_header parsing

--- analyzers/avc/parse.py
+++ analyzers/avc/parse.py
@@ -1,6 +1,8 @@
 import stream
 import syntax
 
+import math
+
 class AVCBitstream(stream.Bitstream):
     def __init__(self, bytes, byte_start):
         super(AVCBitstream, self).__init__(bytes, byte_start)
@@ -21,7 +23,8 @@ class AVCBitstream(stream.Bitstream):
         return val
 
     def get_ue(self, name=None, format=None):
-        self.start_syntax_item()
+        if name:
+            self.start_syntax_item()
 
         val = self.get_exp_golomb()
 
@@ -31,7 +34,7 @@ class AVCBitstream(stream.Bitstream):
                 title = '%s: %s' % (name, format(val))
             else:
                 title = '%s: %i' % (name, val)
-        self.finish_syntax_item(title)
+            self.finish_syntax_item(title)
         return val
 
     def get_se(self, name=None, format=None):
@@ -105,6 +108,7 @@ class NAL:
         reserved_zero_2bits = self.bitstream.getbits(2)
         sps.level_idc = self.bitstream.getbits(8, 'level_idc')
         sps.seq_parameter_set_id = self.bitstream.get_ue('seq_parameter_set_id')
+        sps.separate_colour_plane_flag = 0
         if sps.profile_idc in (100, 110, 122, 244, 44, 83, 86, 118, 128, 138, 139, 134, 135):
             sps.chroma_format_idc = self.bitstream.get_ue('chroma_format_idc')
             if sps.chroma_format_idc == 3:
@@ -161,7 +165,7 @@ class NAL:
         pps.pic_parameter_set_id = self.bitstream.get_ue('pic_parameter_set_id')
         pps.seq_parameter_set_id = self.bitstream.get_ue('seq_parameter_set_id')
         pps.entropy_coding_mode_flag = self.bitstream.getbits(1, 'entropy_coding_mode_flag')
-        pps.pic_order_present_flag = self.bitstream.getbits(1, 'pic_order_present_flag')
+        pps.bottom_field_pic_order_in_frame_present_flag = self.bitstream.getbits(1, 'bottom_field_pic_order_in_frame_present_flag')
         pps.num_slice_groups_minus1 = self.bitstream.get_ue('num_slice_groups_minus1')
         
         if pps.num_slice_groups_minus1 > 0:
@@ -198,7 +202,7 @@ class NAL:
         pps.pic_init_qp_minus26 = self.bitstream.get_se('pic_init_qp_minus26')
         pps.pic_init_qs_minus26 = self.bitstream.get_se('pic_init_qs_minus26')
         pps.chroma_qp_index_offset = self.bitstream.get_se('chroma_qp_index_offset')
-        pps.deblocking_filter_variables_present_flag = self.bitstream.getbits(1, 'deblocking_filter_variables_present_flag')
+        pps.deblocking_filter_control_present_flag = self.bitstream.getbits(1, 'deblocking_filter_control_present_flag')
         pps.constrained_intra_pred_flag = self.bitstream.getbits(1, 'constrained_intra_pred_flag')    
         pps.redundant_pic_cnt_present_flag = self.bitstream.getbits(1, 'redundant_pic_cnt_present_flag')
 
@@ -216,11 +220,195 @@ class NAL:
     def parse_slice_header(self, avc_configuration):
         self.bitstream.start_syntax_item('slice_header')
         slice_header = ParseObject()
+        
         slice_header.first_mb_in_slice = self.bitstream.get_ue('first_mb_in_slice')
         slice_header.slice_type = self.bitstream.get_ue('slice_type', format=enum_slice_type)
         slice_header.pic_parameter_set_id = self.bitstream.get_ue('pic_parameter_set_id')
+        pps = None
+        for p in avc_configuration.avcConfig.pps_nals:
+            if p.pic_parameter_set.pic_parameter_set_id == slice_header.pic_parameter_set_id:
+                pps = p.pic_parameter_set
+                break
+        sps = None
+        for s in avc_configuration.avcConfig.sps_nals:
+            if s.seq_parameter_set.seq_parameter_set_id == pps.seq_parameter_set_id:
+                sps = s.seq_parameter_set
+                break
+
+        if sps.separate_colour_plane_flag == 1:
+            slice_header.colour_plane_id = self.bitstream.getbits(2, 'colour_plane_id')
+        slice_header.frame_num = self.bitstream.getbits(sps.log2_max_frame_num_minus4 + 4, 'frame_num')
+        if not sps.frame_mbs_only_flag:
+            slice_header.field_pic_flag = self.bitstream.getbits(1, 'field_pic_flag')
+            if slice_header.field_pic_flag:
+                slice_header.bottom_field_flag = self.bitstream.getbits(1, 'bottom_field_flag')
+    
+        IdrPicFlag = (self.nal_unit_type == 5)
+        if IdrPicFlag:
+            slice_header.idr_pic_id = self.bitstream.get_ue('idr_pic_id')
+
+        if sps.pic_order_cnt_type == 0:
+            slice_header.pic_order_cnt_lsb = self.bitstream.getbits(sps.log2_max_pic_order_cnt_lsb_minus4 + 4, 'pic_order_cnt_lsb')
+            if pps.bottom_field_pic_order_in_frame_present_flag and not slice_header.field_pic_flag:
+                slice_header.delta_pic_order_cnt_bottom = self.bitstream.get_se('delta_pic_order_cnt_bottom')
+
+        slice_header.delta_pic_order_cnt = [0] * 2
+        if sps.pic_order_cnt_type == 1 and not sps.pic_order_cnt_type:
+            slice_header.delta_pic_order_cnt[0] = self.bitstream.get_se('delta_pic_order_cnt[0]')
+            if pps.bottom_field_pic_order_in_frame_present_flag and not slice_header.field_pic_flag:
+                slice_header.delta_pic_order_cnt[1] = self.bitstream.get_se('delta_pic_order_cnt[1]')
+
+        if pps.redundant_pic_cnt_present_flag:
+            slice_header.redundant_pic_cnt = self.bitstream.get_ue('redundant_pic_cnt')
+
+        if slice_header.slice_type in (1, 6):
+            slice_header.direct_spatial_mv_pred_flag = self.bitstream.getbits(1, 'direct_spatial_mv_pred_flag')
+
+        if slice_header.slice_type in (0, 1, 3, 5, 6, 9):
+            slice_header.num_ref_idx_active_override_flag = self.bitstream.getbits(1, 'num_ref_idx_active_override_flag')
+            if slice_header.num_ref_idx_active_override_flag:
+                slice_header.num_ref_idx_l0_active_minus1 = self.bitstream.get_ue('num_ref_idx_l0_active_minus1')
+                if slice_header.slice_type in (1, 6):
+                    slice_header.num_ref_idx_l1_active_minus1 = self.bitstream.get_ue('num_ref_idx_l1_active_minus1')
+
+        slice_header.ref_pic_list_modification = self.parse_ref_pic_list_modification(slice_header.slice_type)
+        if (pps.weighted_pred_flag and slice_header.slice_type in (0, 3)) or (pps.weighted_bipred_idc and slice_header.slice_type == 1):
+            slice_header.pred_weight_table = self.parse_pred_weight_table(slice_header.slice_type, sps, pps)
+        
+        if self.nal_ref_idc != 0:
+            slice_header.dec_ref_pic_marking = self.parse_dec_ref_pic_marking()
+
+        if pps.entropy_coding_mode_flag and slice_header.slice_type not in (2, 4, 7, 9):   
+            slice_header.cabac_init_idc = self.bitstream.get_ue('cabac_init_idc')
+        slice_header.slice_qp_delta = self.bitstream.get_se('slice_qp_delta')
+
+        if slice_header.slice_type in (3, 4, 8, 9):
+            if slice_header.slice_type == 3:
+                slice_header.sp_for_switch_flag = self.bitstream.getbits(1, 'sp_for_switch_flag')
+            slice_header.slice_qs_delta = self.bitstream.get_se('slice_qs_delta')
+
+        if pps.deblocking_filter_control_present_flag:
+            slice_header.disable_deblocking_filter_idc = self.bitstream.get_ue('disable_deblocking_filter_idc')
+            if slice_header.disable_deblocking_filter_idc != 1:
+                slice_header.slice_alpha_c0_offset_div2 = self.bitstream.get_se('slice_alpha_c0_offset_div2')
+                slice_header.slice_beta_offset_div2 = self.bitstream.get_se('slice_beta_offset_div2')
+
+        if pps.num_slice_groups_minus1 > 0 and pps.slice_group_map_type >= 3 and pps.slice_group_map_type <= 5:
+            SliceGroupChangeRate = pps.slice_group_change_rate_minus1 + 1
+            PicSizeInMapUnits = pps.pic_size_in_map_units_minus1 + 1
+            count = math.ceil(math.log(PicSizeInMapUnits // SliceGroupChangeRate + 1, 2))
+            slice_header.slice_group_change_cycle = self.bitstream.getbits(count, 'slice_group_change_cycle')
+
         self.bitstream.finish_syntax_item()
         return slice_header
+
+    def parse_ref_pic_list_modification(self, slice_type):
+        self.bitstream.start_syntax_item('ref_pic_list_modification')
+        rplm = ParseObject()
+
+        if slice_type % 5 != 2 and slice_type % 5 != 4:
+            ref_pic_list_modification_flag_l0 = self.bitstream.getbits(1)
+            if ref_pic_list_modification_flag_l0:
+                while True:
+                    modification_of_pic_nums_idc = self.bitstream.get_ue()
+                    if modification_of_pic_nums_idc in (0, 1):
+                        abs_diff_pic_num_minus1 = self.bitstream.get_ue('abs_diff_pic_num_minus1')
+                    elif modification_of_pic_nums_idc == 2:
+                        long_term_pic_num = self.bitstream.get_ue('long_term_pic_num')
+                    elif modification_of_pic_nums_idc == 3:
+                        break
+
+        if slice_type % 5 == 1:
+            ref_pic_list_modification_flag_l1 = self.bitstream.getbits(1)
+            if ref_pic_list_modification_flag_l1:
+                while True:
+                    modification_of_pic_nums_idc = self.bitstream.get_ue()
+                    if modification_of_pic_nums_idc in (0, 1):
+                        abs_diff_pic_num_minus1 = self.bitstream.get_ue('abs_diff_pic_num_minus1 (B)')
+                    elif modification_of_pic_nums_idc == 2:
+                        long_term_pic_num = self.bitstream.get_ue('long_term_pic_num (B)')
+                    elif modification_of_pic_nums_idc == 3:
+                        break
+
+        self.bitstream.finish_syntax_item()
+        return rplm
+
+    def parse_pred_weight_table(self, slice_type, sps, pps):
+        self.bitstream.start_syntax_item('pred_weight_table')
+        pwt = ParseObject()
+        pwt.luma_log2_weight_denom = self.bitstream.get_ue('luma_log2_weight_denom')
+        ChromaArrayType = sps.chroma_format_idc if sps.separate_colour_plane_flag == 1 else 0
+        if ChromaArrayType != 0:
+            pwt.chroma_log2_weight_denom = self.bitstream.get_ue('chroma_log2_weight_denom')
+
+        pwt.luma_weight_l0 = [0] * pps.num_ref_idx_l0_active_minus1
+        pwt.luma_offset_l0 = [0] * pps.num_ref_idx_l0_active_minus1
+        pwt.chroma_weight_l0 = [None] * pps.num_ref_idx_l0_active_minus1
+        pwt.chroma_offset_l0 = [None] * pps.num_ref_idx_l0_active_minus1
+        for i in range(pps.num_ref_idx_l0_active_minus1):
+            luma_weight_l0_flag = self.bitstream.getbits(1)
+            if luma_weight_l0_flag:
+                pwt.luma_weight_l0[i] = self.bitstream.get_se('luma_weight_l0[%i]' % i)
+                pwt.luma_offset_l0[i] = self.bitstream.get_se('luma_offset_l0[%i]' % i)
+
+            if ChromaArrayType != 0:
+                chroma_weight_l0_flag = self.bitstream.getbits(1)
+                if chroma_weight_l0_flag:
+                    pwt.chroma_weight_l0[i] = [0] * 2
+                    pwt.chroma_offset_l0[i] = [0] * 2
+                    for j in range(2):
+                        pwt.chroma_weight_l0[i][j] = self.bitstream.get_se('chroma_weight_l0[%i][%i]' % (i, j))
+                        pwt.chroma_offset_l0[i][j] = self.bitstream.get_se('chroma_offset_l0[%i][%i]' % (i, j))
+
+        if slice_type % 5 == 1:
+            pwt.luma_weight_l1 = [0] * pps.num_ref_idx_l1_active_minus1
+            pwt.luma_offset_l1 = [0] * pps.num_ref_idx_l1_active_minus1
+            pwt.chroma_weight_l1 = [None] * pps.num_ref_idx_l1_active_minus1
+            pwt.chroma_offset_l1 = [None] * pps.num_ref_idx_l1_active_minus1
+            for i in range(pps.num_ref_idx_l1_active_minus1):
+                luma_weight_l1_flag = self.bitstream.getbits(1)
+                if luma_weight_l1_flag:
+                    pwt.luma_weight_l1[i] = self.bitstream.get_se('luma_weight_l1[%i]' % i)
+                    pwt.luma_offset_l1[i] = self.bitstream.get_se('luma_offset_l1[%i]' % i)
+
+                if ChromaArrayType != 0:
+                    chroma_weight_l1_flag = self.bitstream.getbits(1)
+                    if chroma_weight_l1_flag:
+                        pwt.chroma_weight_l1[i] = [0] * 2
+                        pwt.chroma_offset_l1[i] = [0] * 2
+                        for j in range(2):
+                            pwt.chroma_weight_l1[i][j] = self.bitstream.get_se('chroma_weight_l1[%i][%i]' % (i, j))
+                            pwt.chroma_offset_l1[i][j] = self.bitstream.get_se('chroma_offset_l1[%i][%i]' % (i, j))
+
+        self.bitstream.finish_syntax_item()
+        return pwt
+
+    def parse_dec_ref_pic_marking(self):
+        self.bitstream.start_syntax_item('dec_ref_pic_marking')
+        drpm = ParseObject()
+
+        IdrPicFlag = (self.nal_unit_type == 5)
+        if IdrPicFlag:
+            drpm.no_output_of_prior_pics_flag = self.bitstream.getbits(1, 'no_output_of_prior_pics_flag')
+            drpm.long_term_reference_flag = self.bitstream.getbits(1, 'long_term_reference_flag')
+        else:
+            adaptive_ref_pic_marking_mode_flag = self.bitstream.getbits(1)
+            if adaptive_ref_pic_marking_mode_flag:
+                while True:
+                    memory_management_control_operation = self.bitstream.get_ue('memory_management_control_operation')
+                    if memory_management_control_operation in (1, 3):
+                        difference_of_pic_nums_minus1 = self.bitstream.get_ue('difference_of_pic_nums_minus1')
+                    elif memory_management_control_operation == 2:
+                        long_term_pic_num = self.bitstream.get_ue('long_term_pic_num')
+                    elif memory_management_control_operation in (3, 6):
+                        long_term_frame_idx = self.bitstream.get_ue('long_term_frame_idx')
+                    elif memory_management_control_operation == 4:
+                        max_long_term_frame_idx_plus1 = self.bitstream.get_ue('max_long_term_frame_idx_plus1')
+                    elif memory_management_control_operation == 0:
+                        break
+
+        self.bitstream.finish_syntax_item()
+        return drpm
 
     def parse_slice_data(self):
         self.bitstream.start_syntax_item('slice_data')
--- analyzers/mp4/avc.py
+++ analyzers/mp4/avc.py
@@ -15,12 +15,14 @@ class AVCDecoderConfigurationRecord:
 
         numOfSequenceParameterSets = bitstream.getbits(5)
         bitstream.start_syntax_item('SequenceParameterSets')
+        self.sps_nals = []
         for i in range(numOfSequenceParameterSets):
             sequenceParameterSetLength = bitstream.getbits(16)
             bitstream.start_syntax_item('%i' % i)
             sps_bytes = bytes[bitstream.pos // 8: bitstream.pos // 8 + sequenceParameterSetLength + 1]
             sps_nal = analyzers.avc.parse.NAL()
             sps_nal.parse(sps_bytes, byte_start + bitstream.pos // 8, sequenceParameterSetLength, None)
+            self.sps_nals.append(sps_nal)
             bitstream.pos += sequenceParameterSetLength * 8
             bitstream.append_syntax_item(sps_nal.syntax_item)
             bitstream.finish_syntax_item()
@@ -28,12 +30,14 @@ class AVCDecoderConfigurationRecord:
 
         numOfPictureParameterSets = bitstream.getbits(8)
         bitstream.start_syntax_item('PictureParameterSets')
+        self.pps_nals = []
         for i in range(numOfPictureParameterSets):
             pictureParameterSetLength = bitstream.getbits(16)
             bitstream.start_syntax_item('%i' % i)
             pps_bytes = bytes[bitstream.pos // 8: bitstream.pos // 8 + pictureParameterSetLength + 1]
             pps_nal = analyzers.avc.parse.NAL()
             pps_nal.parse(pps_bytes, byte_start + bitstream.pos // 8, pictureParameterSetLength, None)
+            self.pps_nals.append(pps_nal)
             bitstream.pos += pictureParameterSetLength * 8
             bitstream.append_syntax_item(pps_nal.syntax_item)
             bitstream.finish_syntax_item()
